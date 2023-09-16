c697d335e5073e4c548e7803d4efb70853a50d13
LEYN <limleyn53@gmail.com>
2023-09-17 04:03:37 +0900

    LMS-Service 초기화 설정 - 2

--- src/main/java/com/triple/lmsservice/LmsServiceApplication.java
+++ src/main/java/com/triple/lmsservice/LmsServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LmsServiceApplication {
 
+    //2023.09.17
+
     public static void main(String[] args) {
         SpringApplication.run(LmsServiceApplication.class, args);
     }
